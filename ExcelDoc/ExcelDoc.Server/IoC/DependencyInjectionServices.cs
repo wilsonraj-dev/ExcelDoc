cd2bec9701ed621d2000b38ce5134aa6bd8fbd52
Wilson Junior <wilson.assis.junior@gmail.com>
2026-04-04 17:25:23 -0300

    Injeção de depedência dos serviços

---
+++ ExcelDoc/ExcelDoc.Server/IoC/DependencyInjectionServices.cs
@@ -0,0 +1,40 @@
+﻿using ExcelDoc.Server.Background;
+using ExcelDoc.Server.Background.Interfaces;
+using ExcelDoc.Server.Repositories;
+using ExcelDoc.Server.Repositories.Interfaces;
+using ExcelDoc.Server.Services;
+using ExcelDoc.Server.Services.Interfaces;
+
+namespace ExcelDoc.Server.IoC
+{
+    public static class DependencyInjectionServices
+    {
+        public static IServiceCollection AddInfrastructureRepositories(this IServiceCollection services)
+        {
+            services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+            services.AddSingleton<ISystemClock, SystemClock>();
+
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<IConfiguracaoRepository, ConfiguracaoRepository>();
+            services.AddScoped<IDocumentoRepository, DocumentoRepository>();
+            services.AddScoped<IColecaoRepository, ColecaoRepository>();
+            services.AddScoped<IProcessamentoRepository, ProcessamentoRepository>();
+
+            services.AddScoped<IEncryptionService, EncryptionService>();
+            services.AddScoped<IHashArquivoService, HashArquivoService>();
+            services.AddScoped<IArquivoStorageService, ArquivoStorageService>();
+            services.AddScoped<IExcelReaderService, ExcelReaderService>();
+            services.AddScoped<IUsuarioAcessoService, UsuarioAcessoService>();
+            services.AddScoped<IDocumentoService, DocumentoService>();
+            services.AddScoped<IConfiguracaoService, ConfiguracaoService>();
+            services.AddScoped<IColecaoService, ColecaoService>();
+            services.AddScoped<IPayloadBuilderService, PayloadBuilderService>();
+            services.AddScoped<ISapServiceLayerClient, SapServiceLayerClient>();
+            services.AddScoped<IProcessamentoService, ProcessamentoService>();
+            services.AddScoped<IProcessamentoWorkerService, ProcessamentoWorkerService>();
+            services.AddHostedService<QueuedProcessingHostedService>();
+
+            return services;
+        }
+    }
+}
--- ExcelDoc/ExcelDoc.Server/Program.cs
+++ ExcelDoc/ExcelDoc.Server/Program.cs
@@ -1,12 +1,23 @@
 using ExcelDoc.Server.Data;
+using ExcelDoc.Server.Options;
 using Microsoft.EntityFrameworkCore;
+using ExcelDoc.Server.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' não configurada.");
 
 // Add services to the container.
 
+builder.Services.Configure<ProcessingOptions>(builder.Configuration.GetSection(ProcessingOptions.SectionName));
+builder.Services.Configure<StorageOptions>(builder.Configuration.GetSection(StorageOptions.SectionName));
+builder.Services.Configure<EncryptionOptions>(builder.Configuration.GetSection(EncryptionOptions.SectionName));
+
 builder.Services.AddDbContext<ExcelDocDbContext>(options =>
-    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseMySQL(connectionString));
+
+builder.Services.AddHttpClient("sap-service-layer");
+builder.Services.AddInfrastructureRepositories();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
