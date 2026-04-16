2058c684ac03091f0ec1f4e4c53c57f0440eb902
Wilson Junior <wilson.assis.junior@gmail.com>
2026-04-16 15:41:39 -0300

    Exclusão componente não usado

--- ExcelDoc/exceldoc.client/src/app/features/colecoes/components/colecoes-home/colecoes-home.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-colecoes-home',
-  templateUrl: './colecoes-home.component.html',
-  styleUrl: './colecoes-home.component.css'
-})
-export class ColecoesHomeComponent {}
